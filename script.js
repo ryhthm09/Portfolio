86530dba9e18235e4a29225977523a31b57e79e5
ryhthm09 <anandsharma1123@gmail.com>
2025-06-08 11:39:47 +0530

    Made a basic portfolio website

---
+++ script.js
@@ -0,0 +1,13 @@
+let dayNight = document.querySelector('.dayNight');
+let banner = document.querySelector('.banner');
+
+dayNight.addEventListener('click', () => {
+  banner.classList.toggle('night');
+});
+let typingEffect = new Typed("#text", {
+  strings: ["Anand", "Web Developer", "Coder", "Researcher"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
+  backDelay: 1000,
+});
